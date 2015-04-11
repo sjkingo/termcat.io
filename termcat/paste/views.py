97f6ae2d293810e6757917530947eef5d5625086
Sam Kingston <sam@sjkwi.com.au>
2015-04-12 05:05:46 +1000

    Add view for showing raw paste

--- termcat/paste/views.py
+++ termcat/paste/views.py
@@ -1,13 +1,21 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import hasher, Paste
 
-def view_paste(request, hashid, template='paste/paste.html'):
+def decode_and_get_paste(hashid):
     decoded_id = hasher.decode(hashid)
     if len(decoded_id) == 0:
         raise Http404()
 
     paste = get_object_or_404(Paste, pk=decoded_id[0])
+    return paste
+
+def view_paste(request, hashid, template='paste/paste.html'):
+    paste = decode_and_get_paste(hashid)
     context = {'paste': paste}
     return render(request, template, context)
+
+def view_paste_raw(request, hashid):
+    paste = decode_and_get_paste(hashid)
+    return HttpResponse(paste.data, content_type='text/plain')
